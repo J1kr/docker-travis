7d4559931e8e8e08c4925c06250a0397ab98d755
mogki <mogki@naver.com>
2021-02-03 17:53:22 +0900

    #Docker Run

---
+++ nodejs-docker-app/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const PORT = 8080;
+
+const app = express();
+app.get('/', (req,res) => {
+    res.send("HI. J1")
+});
+
+app.listen(PORT);
+console.log("Server is running")
